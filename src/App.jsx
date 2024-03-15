0292082c37f0b6971a7cd00a969731a2238b60ef
Chokseys2022 <choksey2022@gmail.com>
2024-03-15 11:26:56 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Cars</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
